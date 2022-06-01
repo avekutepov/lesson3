3878207037c71db264dfe0b7cfa680342ec44a81
avekutepov <kutepov_pasha@list.ru>
2022-06-01 16:27:54 +0600

    for pull request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import FirstComponent from "./components/FirstComponent";
+import NewComponent from "./components/NewComponent";
 
 
 function App() {
   return (
    <div>
        <FirstComponent/>
+       <NewComponent/>
    </div>
   );
 }
